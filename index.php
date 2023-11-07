38f721e68c8f430f7ca746cc2cd8f3a40c8ac7f5
Vitor Kruel <vkruel.programador@gmail.com>
2023-11-07 10:28:57 -0300

    Primeiro arquivo em PHP;

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro Exemplo PHP</title>
+</head>
+
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+        $idade = 3;
+        $salario = 1825.54;
+        $nome = 'Leonardo';
+        $cansado = false;
+
+        echo 'Olá, Mundo!';
+    ?>
+</body>
+
+</html>
